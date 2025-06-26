c81fbfb2f07498136f419cff853adfc9f2d3cedc
sgzinnnnn <mascarenhass832@gmail.com>
2025-06-26 08:34:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const menuHamburguer = document.querySelector('.menu-hamburguer');
 menuHamburguer.addEventListener('click' , () =>{
     toggleMenu();
 });
+
+function toggleMenu(){
+    const nav = document.querySelector('.nav-responsive');
+}
+
+if (menuHamburguer.classList.contains('change')) {
+    nav.style.display = 'block';
+} else {
+    nav.style.display = 'none';
+}
